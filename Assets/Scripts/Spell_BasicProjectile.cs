7f64d9e682718ddbbc062f9a1b485fe5cf3da683
flamegiraffe <flamegiraffes@gmail.com>
2018-05-01 16:02:29 -0700

    lightning and meteor look bomb

--- Assets/Scripts/Spell_BasicProjectile.cs
+++ Assets/Scripts/Spell_BasicProjectile.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class Spell_BasicProjectile : SpellGeneric {
 		public int damage = 10;
-		public GameObject deathPS;
+		public GameObject deathPS1;
+		public GameObject deathPS2;
+		public GameObject deathPS3;
+		public GameObject deathPS4;
 		public override void Trigger(Vector3 v, Duel_PlayerScript p, SpellManagerScript s){
 			p.tryHit(v.z, damage);
-			if(deathPS!=null){
-				deathPS.transform.parent = null;
-				deathPS.GetComponent<ParticleSystem>().Play();
+			if(deathPS1!=null){
+				deathPS1.transform.parent = null;
+				deathPS1.GetComponent<ParticleSystem>().Play();
+			}
+			if(deathPS2!=null){
+				deathPS2.transform.parent = null;
+				deathPS2.GetComponent<ParticleSystem>().Play();
+			}
+			if(deathPS3!=null){
+				deathPS3.transform.parent = null;
+				deathPS3.GetComponent<ParticleSystem>().Play();
+			}
+			if(deathPS4!=null){
+				deathPS4.transform.parent = null;
+				deathPS4.GetComponent<ParticleSystem>().Play();
 			}
 			Destroy(gameObject);
 		}
