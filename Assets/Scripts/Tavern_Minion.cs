853ed2b74335c3ee908d2ddfb785b2908e4a0ba1
flamegiraffe <flamegiraffes@gmail.com>
2018-05-14 20:20:11 -0700

    Market spruced up

---
+++ Assets/Scripts/Tavern_Minion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tavern_Minion : MonoBehaviour {
+	private Animator animator;
+	private float p = 0.5f;
+	private float step;
+	private float lastCheck;
+	private bool leanForward = false;
+	void Start () {
+		animator = GetComponent<Animator>();
+		step = Random.Range(2f, 5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Time.time>lastCheck + step){
+			lastCheck = Time.time;
+			float cf = Random.Range(0f, 1f);
+			if(cf<p){
+				leanForward = !leanForward;
+				animator.SetBool("LeanForward", leanForward);
+			}
+		}
+	}
+}
