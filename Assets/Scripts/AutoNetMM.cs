6958c18bd7824373719b0dc27da367412f8360f3
tavrn <arcadearcane@gmail.com>
2018-05-21 19:56:07 -0700

    lights added to potions

--- Assets/Scripts/AutoNetMM.cs
+++ Assets/Scripts/AutoNetMM.cs
@@ -3,99 +3,129 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
+//using UnityEngine.SceneManagement;
 
 public class AutoNetMM : NetworkLobbyManager {
 
-	void Start () {
-        MMStart();
-        MMListMatches();
+/*
+	void Awake()
+	{
+		//Delagates the function
+	  //SceneManager.activeSceneChanged += OnSceneWasSwitched;
+	}
+
+	void OnSceneWasSwitched()
+	{
+		want everything to be destroyed when you go back to the tavern
+		Debug.Log("OnSceneWasSwitched");
+	  if (SceneManager.GetActiveScene().name == "Tavern")
+		{
+			MMDestroyMatch();
+		}
+	}
+*/
+	void Start ()
+	{
+		//Change this to happen when you go through the door instead
+  	MMStart();
+  	MMListMatches();
 	}
 
 	void MMStart()
+  {
+    Debug.Log("MMStart");
+
+    this.StartMatchMaker();
+  }
+
+  void MMListMatches()
+	{
+    Debug.Log("MMListMatches");
+
+    this.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
+  }
+
+  public override void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+  {
+    Debug.Log("OnMatchList");
+
+    base.OnMatchList(success, extendedInfo, matchList);
+
+    if(!success)
     {
-        Debug.Log("MMStart");
-
-        this.StartMatchMaker();
+      Debug.Log("List failed: " + extendedInfo);
     }
-
-    void MMListMatches()
+    else
     {
-        Debug.Log("MMListMatches");
-
-        this.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
+      if (matchList.Count > 0)
+      {
+        Debug.Log("Listed matches; first: " + matchList[0]);
+        MMJoinMatch(matchList[0]);
+      }
+      else
+      {
+        // no rooms open
+        MMCreateMatch();
+      }
     }
+	}
 
-    public override void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+  void MMJoinMatch(MatchInfoSnapshot firstMatch)
+  {
+    Debug.Log("MMJoinMatch");
+
+    this.matchMaker.JoinMatch(firstMatch.networkId, "", "", "", 0, 0, OnMatchJoined);
+  }
+
+  public override void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+	{
+    Debug.Log("OnMatchJoined");
+
+    base.OnMatchJoined(success, extendedInfo, matchInfo);
+
+    if(!success)
     {
-        Debug.Log("OnMatchList");
-
-        base.OnMatchList(success, extendedInfo, matchList);
-
-        if(!success)
-        {
-            Debug.Log("List failed: " + extendedInfo);
-        }
-        else
-        {
-            if (matchList.Count > 0)
-            {
-                Debug.Log("Listed matches; first: " + matchList[0]);
-                MMJoinMatch(matchList[0]);
-            }
-            else
-            {
-                // no rooms open
-                MMCreateMatch();
-            }
-        }
+      Debug.Log("Failed to join match: " + extendedInfo);
     }
-
-    void MMJoinMatch(MatchInfoSnapshot firstMatch)
+    else
     {
-        Debug.Log("MMJoinMatch");
-
-        this.matchMaker.JoinMatch(firstMatch.networkId, "", "", "", 0, 0, OnMatchJoined);
+      // Success
+      Debug.Log("Successfully joined: " + matchInfo.networkId);
+			//StartClient();
     }
+  }
 
-    public override void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+  void MMCreateMatch()
+  {
+    Debug.Log("MMCreateMatch");
+
+    this.matchMaker.CreateMatch("MM", 2, true, "", "", "", 0, 0, OnMatchCreate);
+  }
+
+  public override void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+  {
+    Debug.Log("OnMatchCreate");
+    base.OnMatchCreate(success, extendedInfo, matchInfo);
+
+    if(!success)
     {
-        Debug.Log("OnMatchJoined");
-
-        base.OnMatchJoined(success, extendedInfo, matchInfo);
-
-        if(!success)
-        {
-            Debug.Log("Failed to join match: " + extendedInfo);
-        }
-        else
-        {
-            // Success
-            Debug.Log("Successfully joined: " + matchInfo.networkId);
-						//StartClient();
-        }
+      Debug.Log("failed to create: " + extendedInfo);
     }
-
-    void MMCreateMatch()
+    else
     {
-        Debug.Log("MMCreateMatch");
+      //success
+      Debug.Log("successful creation: " + matchInfo.networkId);
+			//StartHost();
+  	}
+	}
 
-        this.matchMaker.CreateMatch("MM", 2, true, "", "", "", 0, 0, OnMatchCreate);
-    }
-
-    public override void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
-    {
-        Debug.Log("OnMatchCreate");
-        base.OnMatchCreate(success, extendedInfo, matchInfo);
-
-        if(!success)
-        {
-            Debug.Log("failed to create: " + extendedInfo);
-        }
-        else
-        {
-            //success
-            Debug.Log("successful creation: " + matchInfo.networkId);
-						//StartHost();
-        }
-    }
+/*
+	void MMDestroyMatch()
+	{
+		StopServer();
+		StopClient();
+		StopHost();
+		StopMatchMaker();
+	}
+*/
 }
